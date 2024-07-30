e4fd1aa7dcc77cfca40508e3552142ee3dc5d367
Ash Srinivas <ash@supallama.ai>
2024-07-29 21:42:38 -0700

    Added metadata and Toaster to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
 import { ReactNode } from 'react'
+
+import type { Metadata } from "next";
 import { Bricolage_Grotesque } from 'next/font/google'
 import { Space_Mono } from 'next/font/google'
+
+import { Toaster } from 'react-hot-toast'
+
 import { cn } from '@/lib/utils'
 import './globals.css'
+
+export const metadata: Metadata = {
+  title: "SupaLlama.ai",
+  description: "Test drive LLM apps before writing code",
+};
 
 const fontHeading = Bricolage_Grotesque({
   subsets: ['latin'],
@@ -34,6 +44,7 @@ export default function Layout({ children }: PropsType) {
         )}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   )
